b3b10fcfe6d488d4355c2eca60eee17735886916
berellevy <berellevy@gmail.com>
2020-11-02 15:14:58 -0500

    Home page is an exact copy lol

--- src/components/FindSitterMenu.js
+++ src/components/FindSitterMenu.js
@@ -1,22 +1,80 @@
 import React, { useState } from "react";
-import ReactDatePicker from "react-datepicker";
+import DatePicker from "react-datepicker";
+import DownArrow from "../assets/Icons/DownArrow";
+import RightArrow from "../assets/Icons/RightArrow";
 
 const FindSitterMenu = () => {
+  const addAnHour = () => {
+    const d = new Date();
+    return d.setHours(d.getHours() + 1);
+  };
+
   const [startDate, setStartDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(addAnHour());
+
+  const DateCustomInput = ({ value, onClick }) => {
+    const todayString = new Date().toLocaleDateString();
+    const customValue = value === todayString ? "Today" : value;
+    return (
+      <div className="dropdown" onClick={onClick}>
+        {customValue} <DownArrow />
+      </div>
+    );
+  };
+
+  const TimeCustomInput = ({ value, onClick }) => (
+    <div className="dropdown dropdown-time" onClick={onClick}>
+      {value} <DownArrow />
+    </div>
+  );
 
   return (
-    <div
-      className="background-blur find-sitter-menu"
-      style={{ height: "357px" }}
-    >
-      <p>Choose date</p>
-      <ReactDatePicker
-        className="dropdown"
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
-      <p className="find-sitter-button"> Find Sitters</p>
-    </div>
+    <form className="background-blur find-sitter-menu">
+      <div className="find-sitter-menu-section">
+        <label className="find-sitter-menu-label">Choose date</label>
+        <DatePicker
+          dateFormat="MM/d/yyyy"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          popperPlacement="bottom"
+          customInput={<DateCustomInput />}
+        />
+      </div>
+
+      <div className="find-sitter-menu-section">
+        <label className="find-sitter-menu-label">Select hours</label>
+        <div className="dropdown-time-container">
+          <DatePicker
+            selected={startTime}
+            onChange={(time) => setStartTime(time)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat="h:mm aa"
+            customInput={<TimeCustomInput />}
+          />
+          <hr />
+          <DatePicker
+            selected={endTime}
+            onChange={(time) => setEndTime(time)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat="h:mm aa"
+            customInput={<TimeCustomInput />}
+          />
+        </div>
+      </div>
+
+      <div className="find-sitter-menu-section">
+        <label className="find-sitter-menu-label">Sitters available</label>
+        <div className="find-sitter-button">
+          Find Sitters
+          <RightArrow />
+        </div>
+      </div>
+    </form>
   );
 };
 
