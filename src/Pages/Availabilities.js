2eb1afdd2e9f21ffaa9723feee3fe944901fea01
berellevy <berellevy@gmail.com>
2020-11-03 16:31:01 -0500

    availabilities page displays a list of sitters

--- src/Pages/Availabilities.js
+++ src/Pages/Availabilities.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import qs from 'qs'
 
 
 const Availabilities = ({ location: { search } }) => {
+
+  const [data, setData] = useState(null)
 
   useEffect( () => {
     const fetchSitters = async () => {
       const results = await fetch("https://babysitter-mendel.herokuapp.com/api/v1/sitters")
       const data = await results.json()
       console.log(data);
+      setData(data)
     }
     fetchSitters()
   },[])
+
+  const sittersList = () => data.map(({name}) => <li>{name}</li>)
   
   console.log(qs.parse(search, { ignoreQueryPrefix: true }));
   return (
     <div>
       <h1>Availabilities</h1>
+      <ul>
+        {sittersList()}
+      </ul>
     </div>
   );
 };
