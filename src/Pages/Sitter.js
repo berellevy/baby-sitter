5332026ee13b55dbdf20559e045b3b0afd42ba93
berellevy <berellevy@gmail.com>
2020-11-09 15:22:53 -0500

    middle of sitter page

--- src/Pages/Sitter.js
+++ src/Pages/Sitter.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { BackendDomain } from '../utils/urls';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Header from "../components/Header";
+import { BackendDomain } from "../utils/urls";
 
-const Sitter = ({match, history}) => {
-  const { id } = match.params
-  const [sitter, setSitter] = useState(null)
+const Sitter = ({ match, history }) => {
+  const { id } = match.params;
+  const [sitter, setSitter] = useState(null);
 
   useEffect(() => {
     const fetchSitters = async () => {
@@ -14,14 +17,45 @@ const Sitter = ({match, history}) => {
     fetchSitters();
   }, []);
 
-  const sitterInfo = () => sitter && sitter
-  console.log(sitterInfo());
+  if (sitter) {
+    const {
+      bio,
+      age,
+      contact_name,
+      contact_phone,
+      contact_relation,
+      name,
+      years_of_experience,
+    } = sitter && sitter;
 
-  return (
-    <div>
-      
-    </div>
-  );
-}
+    return (
+      <div className="container">
+        <Header />
+        <BackButton history={history} />
+        <h1>{name}</h1>
+        {/* sitter contact name and relation */}
+        <form className="find-sitter-menu background-blur">
+        <div className="find-sitter-menu-section">
+          <label className="find-sitter-menu-label">
+            {contact_name} ({name}'s {contact_relation})
+          </label>
+          {/* call button */}
+          <Link to={`tel:${contact_phone}`} className="find-sitter-button">
+            Call {contact_phone}
+          </Link>
+        </div>
+        </form>
+        {/* price - Age */}
+        {/* experience */}
+        {/* bio */}
+        {/* references:
+          name, number
+        */}
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
 
 export default Sitter;
