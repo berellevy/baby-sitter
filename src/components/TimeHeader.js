1545b6f40265e6b31d0cb49ea10bf16c4cf6cf72
berellevy <berellevy@gmail.com>
2020-11-18 16:55:52 -0500

    fix date

--- src/components/TimeHeader.js
+++ src/components/TimeHeader.js
@@ -2,14 +2,16 @@ import qs from "qs";
 import moment from "moment";
 import React from "react";
 
-
 const TimeHeader = ({ search }) => {
-  console.log(search);
-  const { day, startTime, duration } = qs.parse(search, { ignoreQueryPrefix: true });
+  const { day, startTime, duration } = qs.parse(search, {
+    ignoreQueryPrefix: true,
+  });
 
-  const formattedDay = moment(day).format("MMM/D");
+  const formattedDay = moment(day, "DD-MM-YYYY").format("MMM/D");
   const formattedStart = moment(startTime, "hh:mm").format("h:mm");
-  const formattedEnd = moment(startTime, "hh:mm").add(duration, "m").format("h:mma");
+  const formattedEnd = moment(startTime, "hh:mm")
+    .add(duration, "m")
+    .format("h:mma");
 
   return <h1>{`${formattedDay}, ${formattedStart}-${formattedEnd}`}</h1>;
 };
