4c985be8552faeea74c769e679e2cec10b06d498
berellevy <berellevy@gmail.com>
2020-11-24 16:24:50 -0500

    switch from duration to end time

--- src/components/SitterAppointments.js
+++ src/components/SitterAppointments.js
@@ -1,18 +1,22 @@
 import React from "react";
 import moment from "moment"
+import { durationToEndTime } from '../utils/DateTime'
 
-const SitterAppointments = ({ appointments }) => (
-  appointments.map(({id, start_time, end_time }) => {
+const SitterAppointments = ({ appointments }) => {
+  return appointments.map(({id, start_time, duration_minutes }) => {
+    const s_t = moment.utc(start_time).format("M/D hh:mma")
+    const e_t = durationToEndTime(moment.utc(start_time), duration_minutes, "M/D hh:mma")
     return (
-      <li key={id} style={{border: "1px solid black", marginBottom: "5px"}}>
-        <p>From: {moment(start_time).format("M/D hh:mma")}</p>
-        <p>To: {moment(end_time).format("M/D h:mma")}</p>
+      <li key={id} style={{border: "1px solid black", marginBottom: "5px", padding: "5px"}}>
+        <p>{id}</p>
+        <p>From: {s_t}</p>
+        <p>To: {e_t}</p>
 
       </li>
     )
   }
 
   )
-)
+}
 
 export default SitterAppointments;
