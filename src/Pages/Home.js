09f373905e629e44261c1ae80b6ae89a1c0ed2a9
berellevy <berellevy@gmail.com>
2020-11-26 22:52:24 -0500

    restructure background

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,23 @@
 import FindSitterMenu from "../components/FindSitterMenu";
 import Header from "../components/Header";
+import img from "../assets/grass_kid_faded.jpeg"
 
 const Home = () => {
-
   return (
-    <div className="home">
-      <div className="opaque">
-        <Header />
+    <>
+      <div id="home-bg">
+        <img src={img} alt="" />
+      </div>
+      <div class="opaque"></div>
+      <div className="container">
+        <Header classes="background-blur" />
         <h1 className="home-message">
-          Find <br /> a Sitter
+          Find <br/>a Sitter
         </h1>
+
         <FindSitterMenu />
       </div>
-    </div>
+    </>
   );
 };
 
