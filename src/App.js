150fe9abc417467fe3e456f48499adfcd4b1f745
berellevy <berellevy@gmail.com>
2020-11-26 22:45:37 -0500

    add components playground

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router";
 import Availabilities from "./Pages/Availabilities";
 import Sitter from "./Pages/Sitter";
 import SignIn from "./Pages/SignIn";
+import Components from "./Pages/Components";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/availabilities" component={Availabilities} />
+        <Route path="/components" component={Components} />
         <Route path="/sitters/:id" component={Sitter} />
         <Route path="/login" component={SignIn} />
         <Route exact path="/" component={Home} />
