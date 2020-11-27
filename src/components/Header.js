ad1e6ed254896eeeed264b6d6314e0125000fa07
berellevy <berellevy@gmail.com>
2020-11-27 13:36:11 -0500

    put google auth buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import { Link } from 'react-router-dom';
 import HomeButton from '../assets/Icons/HomeButton';
+import Login from './Login';
+import Logout from './Logout';
 
 const Header = ({classes}) => {
   return (
     <div className={`header ${classes}`}>
       <HomeButton />
-      <Link to="/login" className="header-text">Sitter Sign In</Link>
+      {/* <Link to="/login" className="header-text">Sitter Sign In</Link> */}
+      <Login />
+      <Logout />
     </div>
   );
 }
