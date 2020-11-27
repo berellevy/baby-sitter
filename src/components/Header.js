485e2050d5effd35fcc2f5fe665ba9f5640d9d22
berellevy <berellevy@gmail.com>
2020-11-26 22:48:56 -0500

    add classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeButton from '../assets/Icons/HomeButton';
 
-const Header = () => {
+const Header = ({classes}) => {
   return (
-    <div className="header background-blur">
+    <div className={`header ${classes}`}>
       <HomeButton />
       <Link to="/login" className="header-text">Sitter Sign In</Link>
     </div>
