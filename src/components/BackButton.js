7f1645fa6c4d2552cdee3021b5f2c477e32b9d99
berellevy <berellevy@gmail.com>
2020-11-30 16:03:58 -0500

    switch to hook history

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import BackArrow from '../assets/Icons/BackArrow';
 
-const BackButton = ({history}) => {
+const BackButton = () => {
+  let history = useHistory()
   return (
     <button className="back-button" onClick={() => history.goBack()}>
       <BackArrow />
