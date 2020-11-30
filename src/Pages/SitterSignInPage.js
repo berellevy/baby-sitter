4c3c82095fd0bd1eb08bb4d8930033258f7b784f
berellevy <berellevy@gmail.com>
2020-11-30 16:09:38 -0500

    rename

---
+++ src/Pages/SitterSignInPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import BackButton from "../components/BackButton";
+import LoginButton from "../components/LoginButton";
+
+const SitterSignInPage = () => {
+  return (
+    <div class="container">
+      <BackButton/>
+      <h1>Baby-Sitter Sign In</h1>
+      <p>Not For Parents. This page is for babysitters to sign in and manage their bookings.</p>
+      <p>No need for parents to sign in, just enter date & time on the home page and choose your sitter.</p>
+      <form>
+        <LoginButton />
+      </form>
+    </div>
+  );
+};
+
+export default SitterSignInPage;
