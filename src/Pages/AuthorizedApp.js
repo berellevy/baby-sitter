1f1f0a3c5b7f51ae2425c3a6881a2695d7404c88
berellevy <berellevy@gmail.com>
2020-12-01 10:28:09 -0500

    add all babysitter routes

--- src/Pages/AuthorizedApp.js
+++ src/Pages/AuthorizedApp.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import SitterPrivateProfile from './SitterPrivateProfile';
+import EditBookingPage from './EditBookingPage';
+import SitterAvailabilitiesWeekdayPage from './SitterAvailabilitiesWeekdayPage';
+import SitterAvailabilitiesWeeklyPage from './SitterAvailabilitiesWeeklyPage';
+import SitterEditProfilePage from './SitterEditProfilePage';
+import SitterHomePage from './SitterHomePage';
 
 const AuthorizedApp = () => {
   return (
     <div className="container">
     <Switch>
-      <Route path="/" component={SitterPrivateProfile} />
+      <Route exact path="/" component={SitterHomePage} /> 
+      <Route path="/edit-profile" component={SitterEditProfilePage} />
+
+      <Route exact path="/my-schedule" component={SitterAvailabilitiesWeeklyPage} />
+      <Route path="/my-schedule/:weekday" component={SitterAvailabilitiesWeekdayPage} />
+
+      <Route path="/edit-booking/:bookingId" component={EditBookingPage} />
     </Switch>
 
     </div>
