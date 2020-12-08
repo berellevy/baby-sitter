97ac03433fadb93d6dbca0878b9d85e5f7d83540
berellevy <berellevy@gmail.com>
2020-12-08 17:13:25 -0500

    use backend auth

--- src/Pages/SitterHomePage.js
+++ src/Pages/SitterHomePage.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useGoogleAuth } from "../auth/useGoogleLogin";
+import { checkBackendToken } from "../auth/useBackendLogin";
 import Header from "../components/Header";
+import { BackendDomain } from "../utils/urls";
 
 const SitterHomePage = () => {
-  const { googleUser } = useGoogleAuth();
-  const { profileObj } = googleUser
+  useEffect(() => {
+    const token = checkBackendToken()
+
+    const request_options = {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "applocation/json",
+        "Accept": "application/json",
+      }
+    }
+    
+    fetch(BackendDomain("sitter"), request_options)
+      .then(res => res.json())
+      .then(console.log)
+  })
+
   return (
     <>
       <Header />
-      <h1>{profileObj.name}</h1>
+      {/* <h1>{profileObj.name}</h1> */}
       <Link to="/edit-profile" className="border-box">edit profile</Link>
       <Link to="/my-schedule" className="border-box">My schedule</Link>
     </>
