f129a0cfa297ab879182643e112af12d1a42146f
berellevy <berellevy@gmail.com>
2020-12-10 11:05:54 -0500

    add fetcher

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,7 +1,30 @@
+import { checkBackendToken } from "../auth/useBackendLogin";
+import { setToken } from "../auth/useBackendLogin";
+
 export const BackendDomain = (postfix) => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:4000/api/v1/" + postfix;
   } else if (process.env.NODE_ENV === "production") {
     return "https://babysitter-mendel.herokuapp.com/api/v1/" + postfix;
   }
+};
+
+export const fetcher = async ({ url, m, h, b, ...rest}) => {
+  
+  const options = {
+    method: m || "GET",
+    headers: h || {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${checkBackendToken()}`,
+    },
+    body: JSON.stringify(b),
+  };
+
+  const response = await fetch(url, {...options, ...rest})
+  const refreshToken = response.headers.get("refreshToken")
+  if (refreshToken) {
+    setToken(refreshToken)
+  }
+  return response
 };
