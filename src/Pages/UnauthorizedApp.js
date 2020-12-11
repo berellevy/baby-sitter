b00a113347819f0402f65dd81d2c6ad6ce115128
berellevy <berellevy@gmail.com>
2020-12-11 13:39:19 -0500

    removed components page

--- src/Pages/UnauthorizedApp.js
+++ src/Pages/UnauthorizedApp.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Availabilities from "./Availabilities";
-import Components from "./Components";
 import Home from "./Home";
 import SitterPublicProfile from "./SitterPublicProfile";
 
@@ -12,7 +11,6 @@ const UnauthorizedApp = () => {
     <div>
       <Switch>
         <Route path="/availabilities" component={Availabilities} />
-        <Route path="/components" component={Components} />
         <Route path="/sitters/:id" component={SitterPublicProfile} />
         <Route path="/login" component={SitterSignInPage} />
         <Route exact path="/" component={Home} />
