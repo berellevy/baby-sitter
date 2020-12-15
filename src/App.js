56922c55cdff81bbec299f324308693b670fa3af
berellevy <berellevy@gmail.com>
2020-12-15 09:57:17 -0500

    add react-modal

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import UnauthorizedApp from "./Pages/UnauthorizedApp";
 import AuthorizedApp from "./Pages/AuthorizedApp";
 import LoadingPage from "./Pages/LoadingPage";
 import { useBackendAuth } from "./auth/useBackendLogin";
+import ReactModal from "react-modal"
 
 function App() {
+  ReactModal.setAppElement('#root');
 
   const { isLoggedIn, isInitialized } = useBackendAuth();
   
