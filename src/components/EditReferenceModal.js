3d5773f39dc6f41424cc1a99c76bd7e72487bc4d
berellevy <berellevy@gmail.com>
2020-12-16 12:35:26 -0500

    edit sitter persists and updates client

---
+++ src/components/EditReferenceModal.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import ReactModal from 'react-modal'
+import { BackendDomain, fetcher } from '../utils/urls';
+
+const EditReferenceModal = ({modalIsOpen, closeModal, data, updateReference}) => {
+  const {id} = data
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const updatedReference = () => ({
+    id,
+    name,
+    phone,
+  })
+
+  useEffect(() => {
+    setName(data.name)
+    setPhone(data.phone)
+  },[modalIsOpen])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // patch new data
+    const url = BackendDomain("references");
+    const response = await fetcher({url, m: "PATCH", b: updatedReference()})
+    const reference = await response.json()
+    // on response success
+    updateReference(reference)
+    // show succes
+    // update parent
+    closeModal();
+    // clear fields
+
+    // on response failure
+    // show error message
+
+  }
+  return (
+    <>
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+      >
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">name</label>
+
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+          <br />
+
+          <label htmlFor="phone">phone</label>
+          <input
+            type="text"
+            name="phone"
+            value={phone}
+            onChange={({ target }) => setPhone(target.value)}
+          />
+          <br />
+
+          <input type="submit" value="add" />
+        </form>
+
+      </ReactModal>
+      
+      
+    </>
+  );
+}
+
+export default EditReferenceModal;
--- src/components/SitterReferences.js
+++ src/components/SitterReferences.js
@@ -2,12 +2,38 @@ import { useFetch } from "../hooks/useFetch";
 import { BackendDomain } from "../utils/urls";
 import DataProvider from "./DataProvider";
 import AddReferenceModal from "./AddReferenceModal";
+import { useState } from "react";
+import EditReferenceModal from "./EditReferenceModal";
 
 const SitterReferences = () => {
   const [references, loading, error, setReferences] = useFetch({
     url: BackendDomain("references"),
   });
+  
+  // EDIT REFERENCE MODAL STUFF
+  // the reference to edit
+  const [referenceToEdit, setReferenceToEdit] = useState({})
+  // open modal and populate data
+  const openEditReference = async (reference) => {
+    setReferenceToEdit(reference)
+    setIsOpen(true)
+  }
+  
+  // modal open and close
+  const [modalIsOpen, setIsOpen] = useState(false)
 
+  function closeModal () {
+    setIsOpen(false)
+  }
+
+  // update reference
+  const updateReference = (reference) => {
+    const updatedReferences = references.map((r) => r.id === reference.id ? reference : r)
+    setReferences(updatedReferences)
+  }
+  
+
+  // ADD REFERENCE STUFF
   const addReference = (reference) => {
     setReferences([...references, reference])
   }
@@ -15,17 +41,27 @@ const SitterReferences = () => {
   return (
     <>
       <AddReferenceModal addReference={addReference}/>
+
       <DataProvider
         data={references}
         render={(reference) => {
           return (
-            <li key={reference.id}>
+            <li
+              key={reference.id}
+              onClick={() => openEditReference(reference)}
+            >
               <p>{reference.name}</p>
               <p>{reference.phone}</p>
               <button>delete</button>
             </li>
           );
         }}
+      />
+      <EditReferenceModal 
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+        data={referenceToEdit}
+        updateReference={updateReference}
       />
     </>
   );
